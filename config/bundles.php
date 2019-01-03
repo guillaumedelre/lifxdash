6555c306a49f940883c1eef04c51800786c8b558
gdelre <gdelre-5com@france24.com>
2019-01-03 21:03:46 +0100

    tmp

---
+++ config/bundles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+];
---
+++ src/Controller/LifxController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use MischiefCollective\ColorJizz\Formats\HSV;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+class LifxController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Client $lifx)
+    {
+        $response = $lifx->get('/v1/lights/all');
+        $jsonEncoder = new JsonEncoder();
+        $data = $jsonEncoder->decode($response->getBody()->getContents(), JsonEncoder::FORMAT);
+        $colors = [];
+        foreach ($data as $light) {
+            $color = new HSV(
+                $light['color']['hue'],
+                $light['color']['saturation'] * 100,
+                $light['brightness'] * 100
+            );
+            $colors[$light['id']] = $color->toHex()->__toString();
+        }
+
+        return $this->render(
+            'dashboard.html.twig',
+            [
+                'lights' => $data,
+                'colors' => $colors,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/lifx/{id}/power", name="power")
+     */
+    public function power(Client $lifx, Request $request)
+    {
+        $lightId = $request->attributes->get('id');
+        $actual = $request->query->get('power');
+        $lifx->put(
+            "/v1/lights/$lightId/state",
+            [
+                RequestOptions::JSON => [
+                    'power' => ($actual == 'on') ? 'off' : 'on',
+                    'fast'  => false,
+                ],
+            ]
+        );
+
+        return $this->redirectToRoute('index');
+    }
+
+    /**
+     * @Route("/lifx/{id}/color/{color}", name="color")
+     */
+    public function color(Client $lifx, Request $request)
+    {
+        $lightId = $request->attributes->get('id');
+        $lightColor = $request->attributes->get('color');
+        preg_match_all('#rgba\((.*),(.*),(.*),(.*)\)$#', $lightColor, $matches);
+        $color = "rgb:{$matches[1][0]},{$matches[2][0]},{$matches[3][0]} brightness:{$matches[4][0]}";
+        $lifx->put(
+            "/v1/lights/$lightId/state",
+            [
+                RequestOptions::JSON => [
+                    'color' => $color,
+                    'fast'  => false,
+                ],
+            ]
+        );
+
+        return $this->redirectToRoute('index', ['id' => $lightId]);
+    }
+
+}
